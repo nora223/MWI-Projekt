3fe6576e5e6b954008b0887a0f8a76ed8ac36411
CaReich <CaReich@ABL0001.abilis.lan>
2014-11-25 17:31:32 +0100

    Änderungen: Text-Datei einlesen

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,8 +1,14 @@
 package menu;
 
+import java.io.BufferedReader;
 import java.io.File;
+import static java.io.FileDescriptor.in;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 import javax.swing.JFileChooser;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -42,9 +48,9 @@ public class Menu {
 
                     if (containsString(help, name)) {
                         name = help;
-                    }else if (containsString(help, pic)) {
+                    } else if (containsString(help, pic)) {
                         pic = help;
-                    }else if(containsString(help, koordinaten)) {
+                    } else if (containsString(help, koordinaten)) {
                         koordinaten = help;
                     }
 //                    System.out.println(help);
@@ -60,7 +66,72 @@ public class Menu {
 
     }
 
-private static boolean containsString(String s, String subString) {
- return s.indexOf(subString) > -1 ? true : false;
- }
+    public static void readSUR() throws FileNotFoundException {
+        File s = null;
+        String txtname = null;
+        String inhalt = null;
+        int intAnzahl = 0;
+        String anzahlSUR = null;
+        List<String> Koordinaten = new ArrayList<String>();
+
+        try {
+            JFileChooser sc = new JFileChooser();
+            int chooseResult = sc.showDialog(null, "Bitte Datei auswählen");
+            if (chooseResult == JFileChooser.APPROVE_OPTION) {
+                s = sc.getSelectedFile();
+                txtname = s.getName();
+                //System.out.println(txtname); Ausgabe des Textnamens/s beinhaltet stattdessen den kompletten Dateipfad
+                BufferedReader in = new BufferedReader(new FileReader(s));
+                String zeile = null;
+                for (int i = 1; i <= 1; i++) {
+                    anzahlSUR = in.readLine();
+                    intAnzahl = Integer.parseInt(anzahlSUR);
+                    //System.out.println(intAnzahl);
+                }
+
+                //System.out.println("______________________");
+                for (int j = 1; j <= intAnzahl; j++) {
+                    zeile = in.readLine();
+                    String [][] daten = cutSnip(zeile, j);
+                    //System.out.println("______________________");
+                    //Koordinaten.add(zeile);
+
+                    for (int i =0; i<4; i++){
+                    System.out.println("Daten: " + daten[j-1][i]);
+                    }
+                    //System.out.println(zeile);
+                }
+
+                // Test Array List Inhaltausgaben
+                //               System.out.println("______________________");
+                //               for(String current: Koordinaten){
+                //                   System.out.println(current);
+                //               }
+            }
+
+        } catch (Exception e) {
+            javax.swing.JOptionPane.showMessageDialog(null, "Datei könnte nicht gelesen werden. " + e);
+            System.out.println("fehler");
+        }
+    }
+
+    private static boolean containsString(String s, String subString) {
+        return s.indexOf(subString) > -1 ? true : false;
+    }
+
+    public static String[][] cutSnip(String zeile, int i) {
+        String [][] erg = new String [i][4];
+        String komma = ",";
+        StringTokenizer st = new StringTokenizer(zeile, komma);
+        
+                int temp = -1;
+
+                while(st.hasMoreTokens()){
+                    temp++;
+                    String help = st.nextToken();
+                    erg[i-1][temp] = help;
+                    
+            }
+        return erg;
+    }
 }
